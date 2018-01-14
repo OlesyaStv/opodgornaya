7f133d5070a64b280d7e4324af70c8c3a5ac37ad
OlesyaStv <olesya.stv@gmail.com>
2018-01-15 00:26:01 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -8,6 +8,7 @@ package ru.job4j;
 */
 
 public class Calculate{
+
 	
 	/***
 	* Конструктор, вывод строки в консоль.
@@ -17,5 +18,16 @@ public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
 	}
+	
+		 
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+ 
 	
 }
