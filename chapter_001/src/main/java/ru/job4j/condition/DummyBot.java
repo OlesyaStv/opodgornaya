8a98585691fc573d7f7c486a9f5e4dcbf8dc88fc
OlesyaStv <olesya.stv@gmail.com>
2018-01-21 01:18:46 +0300

    3.1. Глупый бот. [#31323] _v2

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -2,20 +2,20 @@ package ru.job4j.condition;
 /**
  * @author Olesya Podgornaya
  * @version $Id$
- * @since 20.01.2018
+ * @since 21.01.2018
  */
 public class DummyBot {
-		/**
-		* @param question ������ �� �������.
-		* @return �����.
-		*/
-		public String answer(String question) {
-			if ("Привет, Бот.".equals(question)) {
-				return "Привет, Умник.";
-			} else if ("Пока".equals(question)) {
-				return "До скорой встречи.";
-			} else {
-				return "Это ставит меня в тупик. Спросите другой вопрос.";
-			}
-		}
+    /**
+     * @param question вопрос
+     * @return answer ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, Умник.";
+        } else if ("Пока".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
 }
