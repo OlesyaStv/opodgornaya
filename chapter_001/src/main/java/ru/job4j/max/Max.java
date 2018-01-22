937ed126b5f73d5fc75a2767e6ae4e3f62b2c49f
OlesyaStv <olesya.stv@gmail.com>
2018-01-22 20:25:20 +0300

    3.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,16 @@
+package ru.job4j.max;
+/**
+* @author Olesya Podgornyaa
+* version $Id$
+* since 22.01.2018
+*/
+
+public class Max {
+	
+	
+	
+	public int max(int first, int second) {
+		return first > second ? first : second;
+	}
+	
+}
