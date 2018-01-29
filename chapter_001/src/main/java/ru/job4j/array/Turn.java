8b58ac0c765f02dc4e360cb0ca0273db0f198c30
OlesyaStv <olesya.stv@gmail.com>
2018-01-29 20:15:08 +0300

    5.2. Перевернуть массив. [#4441]_v2

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -5,20 +5,12 @@ import jdk.nashorn.internal.ir.IfNode;
 public class Turn {
 
     public int[] back(int[] array) {
-        int ln = array.length;
-        int doElementa = (ln / 2);
-        if (ln % 2 != 0) {
-            doElementa = (ln - 1) / 2;
-        }
 
-        int z = 1;
-        for (int i = 0; i < doElementa; i++) {
+        for (int i = 0; i < (array.length/2); i++) {
             int vsp = array[i];
-            array[i] = array[ln - z];
-            array[ln - z] = vsp;
-            z += 1;
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = vsp;
         }
-
         return  array;
 
     }
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -17,8 +17,8 @@ public class TurnTest {
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
         Turn trn = new Turn();
-        int[] massive = new int[] {1, 2, 3, 4, 5};
+        int[] massive = new int[] {1, 2, 3, 4, 5, 6, 7};
         int[] arrayrslt = trn.back(massive);
-        assertThat(arrayrslt, is(new int[] {5, 4, 3, 2, 1}));
+        assertThat(arrayrslt, is(new int[] {7, 6, 5, 4, 3, 2, 1}));
     }
 }
