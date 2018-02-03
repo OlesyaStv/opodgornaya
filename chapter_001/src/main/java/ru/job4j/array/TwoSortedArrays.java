09fbd5e9f9a68f3959621f672b6f06024c32c7b0
OlesyaStv <olesya.stv@gmail.com>
2018-02-03 16:55:20 +0300

    Тестовое задание - реализовать копирование из двух массивов в один массив_v2

--- chapter_001/src/main/java/ru/job4j/array/TwoSortedArrays.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoSortedArrays.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class TwoSortedArrays {
 
     public int[] backSortFromTwo(int[] arrayFirst, int[] arraySecond) {
@@ -8,21 +10,13 @@ public class TwoSortedArrays {
         int indexS = 0;
         for (int indexR = 0; indexR < result.length; indexR++) {
             if (indexF < arrayFirst.length && indexS < arraySecond.length) {
-                if (arrayFirst[indexF] <= arraySecond[indexS]) {
-                    result[indexR] = arrayFirst[indexF];
-                    indexF += 1;
-                } else {
-                    result[indexR] = arraySecond[indexS];
-                    indexS += 1;
-                }
-            } else if (indexF < arrayFirst.length) {
-                result[indexR] = arrayFirst[indexF];
-                indexF += 1;
-
-            } else if (indexS < arraySecond.length) {
-                result[indexR] = arraySecond[indexS];
-                indexS += 1;
+                result[indexR] = (arrayFirst[indexF] <= arraySecond[indexS]) ?  arrayFirst[indexF++] : arraySecond[indexS++];
             }
+        }
+        if (indexF < arrayFirst.length) {
+            System.arraycopy(arrayFirst, indexF, result, indexS + indexF, arrayFirst.length - indexF);
+        } else {
+            System.arraycopy(arraySecond, indexS, result, indexS + indexF, arraySecond.length - indexS);
         }
         return result;
     }
