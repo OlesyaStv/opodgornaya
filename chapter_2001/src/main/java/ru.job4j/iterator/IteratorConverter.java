e55682852a76d1961836fe04a1ab8bc12b763ba7
OlesyaStv <olesya.stv@gmail.com>
2018-03-31 10:44:51 +0300

    5.1.1. Итератор для двухмерного массива int[][]_31/03/2018

--- chapter_2001/src/main/java/ru.job4j/iterator/IteratorConverter.java
+++ chapter_2001/src/main/java/ru.job4j/iterator/IteratorConverter.java
@@ -1,60 +1,39 @@
 package ru.job4j.iterator;
 
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class IteratorConverter implements Iterator {
 
     private int[][] values;
-    private int indexI = 0;
-    private int indexJ = 0;
+    private int row = 0;
+    private int cell = 0;
 
     public IteratorConverter(int[][] values){
         this.values = values;
     }
 
-    public int getIndexI() {
-        return indexI;
-    }
-
-    public int getIndexJ() {
-        return indexJ;
-    }
-
-    public void setIndexI(int indexI) {
-        this.indexI = indexI;
-    }
-
-    public void setIndexJ(int indexJ) {
-        this.indexJ = indexJ;
-    }
-
     @Override
     public boolean hasNext() {
-        return values.length > getIndexI() && values[getIndexI()].length > getIndexJ();
+       return (values.length != row && (values.length > row || values[row].length > cell)) ? true : false;
     }
 
     @Override
     public Object next() throws NoSuchElementException{
-        Object ob = null;
-        for (int i = getIndexI(); i < values.length; i++) {
-            for (int j = getIndexJ(); j < values[i].length; j++){
-                ob =  values[i][j];
-                if (j + 1 == values[i].length){
-                    setIndexI(i + 1);
-                    setIndexJ(0);
-                } else if(j + 1 < values[i].length){
-                    setIndexJ(j + 1);
+        int result;
+        if (hasNext()){
+            if (values[row].length > cell){
+                result = values[row][cell++];
+                if (values[row].length == cell){
+                    row++;
+                    cell = 0;
                 }
-                break;
+            }else {
+                result = values[row][cell++];
             }
-            break;
-        }
-
-        if (ob == null) {
+        } else {
             throw new NoSuchElementException();
         }
-        return ob;
+        return result;
     }
 }
