8007d371e4db3996ca6a8f0ef66e61cf4bf3bd31
OlesyaStv <olesya.stv@gmail.com>
2018-04-15 16:43:49 +0300

    5.3.4. Задан связанный список. Определить цикличность_15042018

--- chapter_2001/src/main/java/ru/job4j/cyclicity/Node.java
+++ chapter_2001/src/main/java/ru/job4j/cyclicity/Node.java
@@ -5,24 +5,39 @@ public class Node<T>  {
     Node<T> next;
 
     public boolean hasCycle(Node first) {
-       boolean hC = false;
+       boolean hasCycle = false;
        Node<T>[] nodes = new Node[50];
        Node<T> currentElement = first;
        int cell = 0;
        do {
            for (int i = 0; i < nodes.length; i++) {
              if (nodes[i] != null && nodes[i] == currentElement.next) {
-                 hC = true;
+                 hasCycle = true;
                  break;
              }
            }
-           if (hC) {
+           if (hasCycle) {
                break;
            }
            nodes[cell++] = currentElement;
            currentElement = currentElement.next;
        } while (currentElement.next != null);
-       return hC;
+       return hasCycle;
+    }
+
+    public boolean hasCycleHareTurtle(Node first) {
+        boolean hasCycle = false;
+        Node<T> turtle = first;
+        Node<T> hare = first;
+        while (hare.next != null && hare.next.next != null) {
+            turtle = turtle.next;
+            hare = hare.next.next;
+            if (turtle == hare) {
+                hasCycle = true;
+                break;
+            }
+        }
+        return hasCycle;
     }
 
     public Node(T element) {
--- chapter_2001/src/test/java/ru/job4j/cyclicity/NodeTest.java
+++ chapter_2001/src/test/java/ru/job4j/cyclicity/NodeTest.java
@@ -9,7 +9,6 @@ public class NodeTest {
 
     @Test
     public void whenHasLoop() {
-
         Node first = new Node(1);
         Node two = new Node(2);
         Node third = new Node(3);
@@ -23,7 +22,6 @@ public class NodeTest {
 
     @Test
     public void whenNoLoop() {
-
         Node first = new Node(1);
         Node two = new Node(2);
         Node third = new Node(3);
@@ -33,5 +31,31 @@ public class NodeTest {
         third.next = four;
         four.next = null;
         assertThat(first.hasCycle(first), is(false));
+    }
+
+    @Test
+    public void whenHasCycleHareTurtle() {
+        Node first = new Node(1);
+        Node two = new Node(2);
+        Node third = new Node(3);
+        Node four = new Node(4);
+        first.next = two;
+        two.next = third;
+        third.next = four;
+        four.next = first;
+        assertThat(first.hasCycleHareTurtle(first), is(true));
+    }
+
+    @Test
+    public void whenNoCycleHareTurtle() {
+        Node first = new Node(1);
+        Node two = new Node(2);
+        Node third = new Node(3);
+        Node four = new Node(4);
+        first.next = two;
+        two.next = third;
+        third.next = four;
+        four.next = null;
+        assertThat(first.hasCycleHareTurtle(first), is(false));
     }
 }
