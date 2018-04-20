2440300af3ad32e04bafd22df7089d659b3bc221
OlesyaStv <olesya.stv@gmail.com>
2018-04-20 22:35:23 +0300

    4. Переопределить только equals [#1004]_20042018

--- chapter_2001/src/main/java/ru/job4j/map/User.java
+++ chapter_2001/src/main/java/ru/job4j/map/User.java
@@ -9,12 +9,23 @@ public class User {
     private int children;
     private Calendar birthday;
 
-
     @Override
-    public int hashCode() {
-
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//
+//        return Objects.hash(name, children, birthday);
+//    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
