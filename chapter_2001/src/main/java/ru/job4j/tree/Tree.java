6271510724e19e72083a941b6ebf07abc4659da2
OlesyaStv <olesya.stv@gmail.com>
2018-04-22 17:30:51 +0300

    1. Создать элементарную структуру дерева [#1711]_22042018

---
+++ chapter_2001/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,98 @@
+package ru.job4j.tree;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree {
+
+    private Node<E> root;
+    private int size;
+    private int wasTaken;
+    private Node<E> lastTakenElement;
+
+    @Override
+    public boolean add(Comparable parent, Comparable child) {
+        boolean wasAdded = false;
+        boolean elementExists = false;
+        Optional<Node> elementFind = findBy(parent);
+        Node<E> parentFind = (Node<E>) elementFind.get();
+        for (Node<E> element: parentFind.leaves()) {
+            if (element.eqValue((E) child)) {
+                elementExists = true;
+                break;
+            }
+        }
+        if (parentFind != null && !elementExists) {
+            parentFind.add(new Node(child));
+            wasAdded = true;
+            size++;
+        }
+        return wasAdded;
+    }
+
+    @Override
+    public Optional<Node> findBy(Comparable value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(((E) value))) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return (Optional) rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            @Override
+            public boolean hasNext() {
+                return wasTaken < size;
+            }
+
+            @Override
+            public E next() {
+                if (lastTakenElement == null) {
+                    lastTakenElement = root;
+                } else {
+                    Optional<Node<E>> rsl = Optional.empty();
+                    Queue<Node<E>> data = new LinkedList<>();
+                    data.offer(root);
+                    boolean findNextElement = false;
+                    while (!data.isEmpty()) {
+                        Node<E> el = data.poll();
+                        if (findNextElement) {
+                            rsl = Optional.of(el);
+                            lastTakenElement = rsl.get();
+                            break;
+                        }
+                        if (el.eqValue(((E) lastTakenElement.getValue()))) {
+                            findNextElement = true;
+                        }
+                        for (Node<E> child : el.leaves()) {
+                            data.offer(child);
+                        }
+                    }
+                }
+                return lastTakenElement.getValue();
+            }
+        };
+    }
+
+
+    public Tree(E root) {
+        this.root = new Node(root);
+        this.size = 0;
+        this.wasTaken = 0;
+        this.lastTakenElement = null;
+    }
+}
---
+++ chapter_2001/src/test/java/ru/job4j/tree/TreeTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class TreeTest {
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void whenGetNext() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        Iterator iterator = tree.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        assertThat(iterator.next(), is(6));
+    }
+}
